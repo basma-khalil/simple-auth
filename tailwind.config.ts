732ff64ba7ec82538a29f2765d5e5dc832a4bbd6
Basma Khalil <basma.mahmoud.khalil@gmail.com>
2024-02-03 05:50:18 +0200

    style: Add project base style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/views/**/*.html"],
+  content: ["./src/views/**/*.{html,js,ts}"],
   theme: {
     extend: {
       fontFamily: {
         mainFontFamily: ["Poppins", "Almarai", "sans-serif"],
       },
-      colors: {},
+      colors: {
+        mainFont: "#333",
+        main: "#4481eb",
+        minor: "#5995fd",
+        gradient: "#04befe",
+      },
+      backgroundImage: {
+        "gradient-45":
+          "linear-gradient(-45deg, var(--tw-gradient-from)0%, var(--tw-gradient-from)100%)",
+      },
     },
   },
   plugins: [],
