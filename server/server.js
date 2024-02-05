9b7cf5dae46d62b1b7cb8925809818b075414640
Basma Khalil <basma.mahmoud.khalil@gmail.com>
2024-02-05 11:07:07 +0200

    fix: Update github actions

--- server/server.js
+++ server/server.js
@@ -1,13 +1,9 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 const app  = express();
 const port = 3000;
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-app.use(express.static(path.join(__dirname, 'build'), {
+app.use(express.static('build', {
     extensions: ['html'],
 }));
 
